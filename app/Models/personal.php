beb3663eba7c6952d13bc61d5e0bd8ce6cab216e
yecasu182 <yecasu182@gmail.com>
2023-07-31 16:40:14 -0500

    se crea datos de la primera tarjeta de consulta, también se creo base de datos, se migro la tabla con sus respectivos campos

---
+++ app/Models/personal.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Usuario extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'nombre',
+        'cargo',
+        'cedula',
+        'foto',
+    ];
+
+}
